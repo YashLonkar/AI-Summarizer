ce7618c61352863fb5f45612ec7b1b32a6748f11
YashLonkar <yashlonkar99@gmail.com>
2024-06-03 20:51:41 +0530

    Fixed Deployment Issues

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,44 @@
 import React from "react";
-import {logo } from '../assets';
-const Hero =() => {
-    return (
-        <header className="w-full flex justify-center items-center flex-col">
-            <nav className="flex justify-between items-center flex-row w-full mn-10 pt-3">
-                <img src={logo} alt="sumz-logo" className="w-28 object-contain"></img>
-                <div>
-                <button type="button" onClick={() => window.open('github.com')} className="black_btn mr-1.5">
-                    GitHub
-                </button>
-                <button type="button" onClick={() => window.open('github.com')} className="black_btn">
-                    LinkedIn
-                </button>
-                </div>
-                
-            </nav>
 
-            <h1 className="head_text">
-                Summarize Articles with <br className="max-md:hidden"/>
-                <span className="orange_gradient">OpenAI GPT-4</span>
-            </h1>
+import { logo } from "../assets";
 
-            <h2 className="desc">
-            Simplify your reading with Summize, an open-source article summarizer
+const Hero = () => {
+  return (
+    <header className='w-full items-center flex flex-col'>
+      <nav className='flex justify-between w-full mb-10 pt-3'>
+        <div className="font-bold text-3xl">SUMMARIZER</div>
+        <div className="ml-auto">
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://www.linkedin.com/in/yashlonkar55/", "_blank")
+          }
+          className='black_btn mr-2'
+        >
+          LinkedIn
+        </button>
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/YashLonkar55/", "_blank")
+          }
+          className='black_btn'
+        >
+          GitHub
+        </button>
+        </div>
+      </nav>
+
+      <h1 className='head_text'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='orange_gradient '>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
         that transforms lengthy articles into clear and concise summaries
-            </h2>
-        </header>
-    );
-}
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/services/store.js
+++ src/services/store.js
@@ -1,14 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import { buildGetDefaultEnhancers } from "@reduxjs/toolkit/dist/react";
-// import { buildGetDefaultMiddleware } from "@reduxjs/toolkit/dist/react";
+
 import { articleApi } from "./article";
-
-
-//Store is a global state and we are using reducer to take particular state/part of that whole part
 
 export const store = configureStore({
     reducer: {
-        [articleApi.reducerPath]: articleApi.reducer
-    }, //grab some part of entire state
-    middleware: (buildGetDefaultMiddleware) => buildGetDefaultMiddleware().concat()
-});
+        [articleApi.reducerPath]: articleApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(articleApi.middleware)
+})
